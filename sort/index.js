d735cb578ee4f4ae1c915819eafef4b39b07dfa4
stein <906060812@qq.com>
2017-03-09 09:35:52 +0800

    bubbleSort complete

---
+++ sort/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/3/9.
+ */
+// import bubbleSort from './bubbleSort';
+let bubbleSort = require('./bubbleSort');
+var arr=[3,44,38,5,47,15,36,26,27,2,46,4,19,50,48];
+console.log(bubbleSort.bubbleSort(arr));
+console.log(bubbleSort.bubbleSortWithPos(arr));
+console.log(bubbleSort.twoWayBubbleSort(arr));
